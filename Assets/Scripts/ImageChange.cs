11989a20a784ec02ffd13d7ee8e7bbbe021c230c
Sulfur <rc7604@g.rit.edu>
2018-02-04 15:45:31 -0500

    Summary: connect character select to the uitest. Also it will determin boy or girl to use in the mainscene. Also have cost, value, quantity text hooked up to display.

---
+++ Assets/Scripts/ImageChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageChange : MonoBehaviour {
+
+	public Image changeImage;//the image that will be change to the idol
+	// Use this for initialization
+	public Sprite girlD;//girl idol default
+	public Sprite boyD;//boy diol default
+	void Start () {
+		if (PlayerPrefs.GetInt ("isGirl") == 1) {
+			changeImage.sprite = girlD;
+		} else if (PlayerPrefs.GetInt ("isGirl") == 0) {//default also will show a boy
+			changeImage.sprite = boyD;
+		}
+	
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
