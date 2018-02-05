4fb6c3b6a7ac8ae3a0762cb61ba76d08ffdcb5eb
James Taggart <jxt1607@g.rit.edu>
2018-02-05 18:35:19 -0500

    Animations, baby!

---
+++ Assets/Scripts/ButtonAnimationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimationScript : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+    public void OnButtonClicked()
+    {
+        animator.SetTrigger("Clicked");
+    }
+}
