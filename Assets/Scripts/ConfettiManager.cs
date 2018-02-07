534c071d0afa6cac3b506b3972ca035202f0aef5
James Taggart <jxt1607@g.rit.edu>
2018-02-07 15:05:05 -0500

    We got particles now

---
+++ Assets/Scripts/ConfettiManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfettiManager : MonoBehaviour {
+
+    public ParticleSystem[] particles;
+    public Transform confettiOrigin;
+    Transform ourPosition;
+    
+
+	// Use this for initialization
+	void Start () {
+        ourPosition = GetComponent<Transform>();
+    }
+	
+	
+    public void OnActivate()
+    {
+        //On click give us back a random index for the confetti to be sent out.
+        int index = Random.Range(0, 9);
+
+        //Move us to mouse position
+        ourPosition.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0);
+
+        //Make it wherever origin says to.
+        Instantiate<ParticleSystem>(particles[index], confettiOrigin);
+    }
+
+
+}
---
+++ Assets/Scripts/ConfettiSelfDestructScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfettiSelfDestructScript : MonoBehaviour {
+
+    public float life = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(GetComponent<GameObject>(), life);
+	}
+	
+}
