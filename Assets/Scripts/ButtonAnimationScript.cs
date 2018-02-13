95160c89fb883d6e00f432c8ab7f99ea5eb82402
Jeffrey Karger <whichazelnj13@aol.com>
2018-02-13 17:26:12 -0500

    tracker for which mood to use in place

--- Assets/Scripts/ButtonAnimationScript.cs
+++ Assets/Scripts/ButtonAnimationScript.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class ButtonAnimationScript : MonoBehaviour {
 
+    enum IdolMood { Bored, Happy, Normal };
+
     Animator animator;
+    private float thisTime;             //counter for time between mood changes
+    private int numClicksMoodChange;    //number of times the button has been clicked since last mood change
+    public int happyThreshold;          //number of clicks needed to move to happy mood
+    private IdolMood currentMood;
 
 	// Use this for initialization
 	void Start () {
+        thisTime = 0;
+        numClicksMoodChange = 0;
+        currentMood = IdolMood.Normal;
         animator = GetComponent<Animator>();
 	}
+
+    void Update()
+    {
+        //increment timer
+        thisTime++;
+
+        //as long as the button has been clicked once and at least 4 seconds have passed, handle clickbased changes
+        if (thisTime >= 240 && numClicksMoodChange != 0)
+        {
+            //check if large amounts of clicking done in the past 4 seconds
+            if (numClicksMoodChange >= happyThreshold && currentMood != IdolMood.Happy)
+            {
+                //change to happy mood
+                HappyIdol();
+            }
+            else if (currentMood != IdolMood.Normal)
+            {
+                //change to normal mood
+                NormalIdol();
+            }
+            //reset timer and numClicksMade
+            thisTime = 0;
+            numClicksMoodChange = 0;
+        }
+        else if (thisTime >= 240 && currentMood != IdolMood.Bored)
+        {
+            //if the button has never been clicked and 20 seconds passed, make the idol bored
+            BoredIdol();
+
+            //reset timer
+            thisTime = 0;
+        }
+    }
 	
     public void OnButtonClicked()
     {
         animator.SetTrigger("Clicked");
+        numClicksMoodChange++;
+    }
+
+    //change the idol image ot the bored image
+    private void BoredIdol()
+    {
+        //change mood
+        currentMood = IdolMood.Bored;
+
+        //change image
+        Debug.Log("I'm so bored, click a little");
+    }
+
+    //change the idol image to the normal image
+    private void NormalIdol()
+    {
+        //change mood
+        currentMood = IdolMood.Normal;
+
+        //change image
+        Debug.Log("Oh good, you are actually playing");
+    }
+
+    //change the idol image to the happy image
+    private void HappyIdol()
+    {
+        //change mood
+        currentMood = IdolMood.Happy;
+
+        //change image
+        Debug.Log("Click me even more");
     }
 }
